99397f536fc4fe96897eadfa40755496267790d4
prachimehta203 <pnmehta203@gmail.com>
2020-08-16 15:28:29 +0530

    model of user with category

--- django/cfg_70/main/models.py
+++ django/cfg_70/main/models.py
@@ -1,5 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class UserProfile(models.Model):
+    CATEGORIES = (  
+    ('1', 'IT'),
+    ('2', 'Operation'),
+    ('3', 'HR'),
+    ('4', 'Audit'),
+    ('5', 'Account'),
+    ('6', 'HOD'),
+    ('7', 'Admin'),
+    ('8', 'ENC'),
+)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    category = models.CharField(max_length=9, choices=CATEGORIES)
+
+    def __str__(self):
+        return self.user.username
